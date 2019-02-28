6025fff0e035d26d87ef967118a39166c1cce8f3
Krivokrysenko <ckrivokrysenko@gmail.com>
2019-02-28 13:09:05 -0500

    My part is done, testing is a bit tough but I can explain how if needed

--- src/server.py
+++ src/server.py
@@ -1,9 +1,5 @@
 import bottle
-"""
-import process
-import process2
-import process3
-"""
+import JSONmaker
 
 from bottle import route, run, static_file, request
 
@@ -18,20 +14,16 @@ def main():
     return static_file("main.js", root="")
 
 
-@route("/grid.csv")
+@route("/grid.csv")  # MAY BE UNNEEDED
 def grid():
     return static_file("grid.csv", root="")
 
 
 @route("/JSONmaker")
 def JSONmaker():
-    return static_file("JSONmaker", root="")
+    return JSONmaker.csvtoarray()
 
 """
-@route("/process")
-def processing():
-    return process.reformat(10, 10)
-
 @route("/process2")
 def processing2():
     sq = request.query["sq"]
