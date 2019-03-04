90a6904f5c4437e8da595dc413a2db1971edb664
Asifur <arahman9@buffalo.edu>
2019-03-04 15:43:31 -0500

    takes in a colorGroup, which can either be red, green, or blue. gives a random color based off group

--- src/randomColorGen.py
+++ src/randomColorGen.py
@@ -1,23 +1,27 @@
-from random import randint
+import random
+
 def randomColorGen(ColorGroup):
-    red = 0
-    blue = 0
-    green = 0
+    red = random.randint(0, 255)
+    blue = random.randint(0, 255)
+    green = random.randint(0, 255)
+    print((red,green,blue))
     dic = {"red": 0, "blue": 1, "green": 2}
-    if dic[ColorGroup] == 0:
+    if ColorGroup == 0:
         while (red < blue) or (red < green):
-            red = randint(0,255)
-            blue = randint(0,255)
-            green = randint(0,255)
-    elif dic[ColorGroup] == 1:
+            red = random.randint(0, 255)
+            print(red)
+            blue = random.randint(0, 255)
+            green = random.randint(0, 255)
+    elif ColorGroup == 1:
         while (blue < red) or (blue < green):
-            red = randint(0,255)
-            blue = randint(0,255)
-            green = randint(0,255)
+            red = random.randint(0, 255)
+            blue = random.randint(0, 255)
+            green = random.randint(0, 255)
     else:
         while (green < red) or (green < blue):
-            red = randint(0,255)
-            blue = randint(0,255)
-            green = randint(0,255)
-    color = '#%02x%02x%02x' % (red, green, blue)
+            red = random.randint(0,255)
+            blue = random.randint(0,255)
+            green = random.randint(0,255)
+    color = (red, green, blue)
+    print(color)
     return color
